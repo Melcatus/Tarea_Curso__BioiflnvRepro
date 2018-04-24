20a004db94b4c0acfd224ff260566f77791442a2
Melania Vega <36024079+Melcatus@users.noreply.github.com>
2018-04-24 01:26:02 -0500

    Update maices.R

--- maices.R
+++ maices.R
@@ -1,3 +1,4 @@
+#convertir el archivo txt en csv para evitar los problemas de acentos y espacios que hay en la base de datos
 #elegir como working directory "BioinfinvRepro/Unidad7/Prac_Uni7/maices/meta/bin"
 #Pregunta 1.1 cargar como df el archivo de maiz-teocintle
 fullmat <-read.csv ("C:/Users/Mel/Desktop/BioinfinvRepro-master/Unidad7/Prac_Uni7/maices/meta/maizteocintle_SNP50k_meta_extended.csv",head = TRUE, sep = ",", na.strings = NULL)
@@ -9,12 +10,12 @@ nrow(fullmat)
 #Pregunta 1.5 contar los estados que fueron muestreados =20
 summary(fullmat$Estado)
 #Pregunta 1.6 muestras que fueron colectadas antes 1980 = 8
-summary(fullmat$AÌ.o._de_colecta)
-#Pregunta 1.7 visualizar cuántas muestras hay por cada raza
+summary(fullmat$AÃŒ.o._de_colecta)
+#Pregunta 1.7 visualizar cuÃ¡ntas muestras hay por cada raza
 summary(fullmat$Raza)
 #Pregunta 1.8 calcular el promedio de altitud en la que se colectaron las muestras
 mean(fullmat$Altitud)
-#Pregunta 1.9 calcular la altitud máxima y mínima en las que se hizo la colecta
+#Pregunta 1.9 calcular la altitud mÃ¡xima y mÃ­nima en las que se hizo la colecta
 range(fullmat$Altitud)
 #Pregunta 1.10 hacer una nueva data frame que incluya solo la raza olotillo
 olotillo <-fullmat[fullmat$Raza=="Olotillo",]
